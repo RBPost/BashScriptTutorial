c01503d2353f53b6826bde49f7b6b5325080fc25
Ryan Postma <rpostma@scalecomputing.com>
2018-05-02 17:55:17 -0400

    created a script that plays with default variables to see how they work.

---
+++ defaultVariables.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# script to highlight variabel functions within #!/usr/bin/env bash
+
+echo "name of the script running is: $0"
+echo
+
+echo "The user running this is $USER"
+echo
+
+echo "host name of this machine is: $HOSTNAME"
+echo
+
+echo "Going to wait 5 sconds now..."
+sleep 5
+
+echo "This is how long has passed since script ran: $SECONDS"
+echo
+
+echo "Generating a random number: $RANDOM"
+echo
+
+echo "This is line number: $LINENO"
+echo
